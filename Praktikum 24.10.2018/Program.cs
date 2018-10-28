80cf98f1719b9a4bf4101a7b48101a21bb2ee655
TugbaAtici <tugba.atici@web.de>
2018-10-28 17:54:52 +0100

    refresh

---
+++ Praktikum 24.10.2018/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Praktikum_24._10._2018
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var i = 4;
+            var pi = 3.1415;
+            var salute = "Hello,World";
+            Console.WriteLine(i+pi+salute);
+        }
+    }
+}
