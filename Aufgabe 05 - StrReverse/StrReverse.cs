aa3aa60835192980ca057474d7e88e5e6d77b2bb
TugbaAtici <tugba.atici@web.de>
2018-11-19 16:13:52 +0100

    StrReverse_Aufgabe05

---
+++ Aufgabe 05 - StrReverse/StrReverse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+class Program
+{
+    static void Main(string[] args)
+    {
+        string text = "My name is Tugba Atici";
+
+        Console.WriteLine(string.Join(" ", text.Split(' ').Reverse()));
+        
+    }
+}
+}
