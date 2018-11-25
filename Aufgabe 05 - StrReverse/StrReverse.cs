83e598ba2c00f7f109d39cdfb9eb7c6bde0a29fc
TugbaAtici <tugba.atici@web.de>
2018-11-25 17:17:43 +0100

    Aufgabe_05_Aktualisiert

--- Aufgabe 05 - StrReverse/StrReverse.cs
+++ Aufgabe 05 - StrReverse/StrReverse.cs
@@ -10,10 +10,56 @@ class Program
 {
     static void Main(string[] args)
     {
-        string text = "My name is Tugba Atici";
-
-        Console.WriteLine(string.Join(" ", text.Split(' ').Reverse()));
+        //string text = "My name is Tugba Atici";
+        LettersReversed(args);
+        OrderOfWordsReversed(args);
+        AllReversed(args);
         
     }
+
+    static void LettersReversed(String[] args)
+        {
+            string input = "";
+
+            for(int i = 0; i < args.Length; i++)
+            {
+                for(int j = args[i].Length - 1; j >= 0; j--)
+                {
+                    input = input + args[i][j];
+                }
+
+                input = input + " ";
+            }
+
+            Console.WriteLine(input);
+        }
+
+     static void OrderOfWordsReversed(String[] args)
+        {
+            string input = "";
+
+            for(int i = args.Length -1; i >= 0; i--)
+            {
+                input = input + args[i] + " ";
+            }
+            Console.WriteLine(input);
+        }
+
+        static void AllReversed(String[] args)
+        {
+            string input = "";
+
+            for(int i = args.Length -1; i >= 0; i--)
+            {
+                for(int j = args[i].Length - 1; j >= 0; j--)
+                {
+                    input = input + args[i][j];
+                }
+
+                input = input + " ";
+            }
+
+            Console.WriteLine(input);
+        }      
 }
 }
