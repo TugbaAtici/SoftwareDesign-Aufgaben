ce5c035577b9158b8c6b7c98ff9ebf42ee11a223
TugbaAtici <tugba.atici@web.de>
2018-12-16 12:48:03 +0100

    Aufgabe09_SaveQuestions_reupload

--- Aufgabe09_SaveQuestions/Program.cs
+++ Aufgabe09_SaveQuestions/Program.cs
@@ -10,17 +10,17 @@ namespace Aufgabe09_SaveQuestions
             
         }
 
-        static void Serialize(string addUserQuestion, int addHowManyAnswers, string userAnswer1, int score)
+        static void Serialize(string addQuestions, int addAnswers, string answersOfUser, int score)
         {
-            System.Xml.Serialization.XmlSerializer x = new System.Xml.Serialization.XmlSerializer(addUserQuestion.GetType());
+            System.Xml.Serialization.XmlSerializer x = new System.Xml.Serialization.XmlSerializer(addQuestions.GetType());
             FileStream str = new FileStream(@"C:\Users\Admin\Documents\GitHub\Modulare Softwareentwicklung im Frontend\Modulare-Softwareentwicklung-im-Frontend\SoftwareDesign-Aufgaben\Aufgabe 08_QuizCodetest.xml", FileMode.Open);
-            x.Serialize(str, addUserQuestion);
+            x.Serialize(str, addQuestions);
 
-            System.Xml.Serialization.XmlSerializer y = new System.Xml.Serialization.XmlSerializer(addHowManyAnswers.GetType());
-            y.Serialize(str, addHowManyAnswers);
+            System.Xml.Serialization.XmlSerializer y = new System.Xml.Serialization.XmlSerializer(addAnswers.GetType());
+            y.Serialize(str, addAnswers);
 
-            System.Xml.Serialization.XmlSerializer z = new System.Xml.Serialization.XmlSerializer(userAnswer1.GetType());
-            z.Serialize(str, userAnswer1);
+            System.Xml.Serialization.XmlSerializer z = new System.Xml.Serialization.XmlSerializer(answersOfUser.GetType());
+            z.Serialize(str, answersOfUser);
 
             Program p = new Program();
             p.QuizMenu(score);
